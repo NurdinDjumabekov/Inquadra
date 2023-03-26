737f94a9566f1f0222709e82713a01c057299721
Nurdin Djumabekov <dui.nurdin@gmail.com>
2023-03-26 22:37:29 +0600

    Add files via upload

---
+++ DZ3_Nurdin_21-03/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import MainPage from "./pages/mainPage/MainPage";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <MainPage />
+      </div>
+    </>
+  );
+}
+
+export default App;
