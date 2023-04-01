7483ef381b1d867bdada6dfc6ccf44e104f38ca2
Nurdin Djumabekov <dui.nurdin@gmail.com>
2023-04-01 15:44:44 +0600

    Delete Month_5-20-03-HW-1_React directory

--- Month_5-20-03-HW-1_React/my-app/src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Footer extends Component {
-  render() {
-    return <footer {...this.props}>Footer</footer>;
-  }
-}
-
-export default Footer;
--- Month_5-20-03-HW-1_React/my-app/src/components/Main/Main.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Main extends Component {
-  render() {
-    return <main {...this.props}>Main</main>;
-  }
-}
-
-export default Main;
