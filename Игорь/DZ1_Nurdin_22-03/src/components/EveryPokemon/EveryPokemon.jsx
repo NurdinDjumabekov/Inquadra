1bf0064b048f0833c51163e188cb1415ed994234
Nurdin Djumabekov <dui.nurdin@gmail.com>
2023-04-21 21:48:48 +0600

    Add files via upload

--- Игорь/DZ1_Nurdin_22-03/src/components/EveryPokemon/EveryPokemon.jsx
+++ Игорь/DZ1_Nurdin_22-03/src/components/EveryPokemon/EveryPokemon.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
 const EveryPokemon = ({ poki }) => {
+  useEffect(() => {}, []);
   return (
     <NavLink to={`/pokemon/${poki.name}`}>
       <ul>
